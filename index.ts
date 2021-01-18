f9c0683193d6032067d2df001dc774a70dc62c40
Atique Ahmed <atique.ahmed@asianetnews.in>
2021-01-18 17:41:29 +0530

    lambda deployment using type script

--- index.ts
+++ index.ts
@@ -1,30 +1,35 @@
 import cors from "cors";
+import { eventContext } from 'aws-serverless-express/middleware';
 import express from 'express';
-
 const bodyParser = require('body-parser')
 const mongoose = require('./db.ts');
 
-const app = express();
 
+export function configureApp() {
+    const app = express();
 
-app.use(cors())
+    app.use(cors())
+    app.use(eventContext());
 
-app.use(bodyParser.json({limit: '5mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '5mb', extended: true}))
+    app.use(bodyParser.json({limit: '5mb', extended: true}))
+    app.use(bodyParser.urlencoded({limit: '5mb', extended: true}))
 
-const apiRoutes = require('./routes/routes');
+    const apiRoutes = require('./routes/routes');
 
-app.use('', apiRoutes);
+    app.use('', apiRoutes);
 
-//Capture All 404 errors
-app.use(function (req,res,next){
-	res.status(404).send('Error - Unable to find the requested resource!');
-});
+    //Capture All 404 errors
+    app.use(function (req,res,next){
+      res.status(404).send('Error - Unable to find the requested resource!');
+    });
 
-app.use((req, res, next) => {
-  req.socket.on('error', () => {});
-  next();
-});
+    app.use((req, res, next) => {
+      req.socket.on('error', () => {});
+      next();
+    });
+    
+    return app;
+}
 
-
-module.exports = app;
+//exports = app;
+// module.exports = app;
--- local.ts
+++ local.ts
@@ -1,10 +1,11 @@
 import config from 'config';
+import {configureApp} from './index';
 
 const chalk = require('chalk');
-const app = require('./index');
+// const app = require('./index');
 const port = process.env.PORT || config.get('server.port');
 
 // Server
-app.listen(port, () => {
+configureApp().listen(port, () => {
   console.log(chalk.blueBright("Server is listening port number:", port));
 })
