f8a9225dc1ae45ea304c85be3f001b911a93eef3
Atique Ahmed <atique.ahmed@asianetnews.in>
2021-01-19 12:54:55 +0530

    changers

--- index.ts
+++ index.ts
@@ -5,31 +5,27 @@ const bodyParser = require('body-parser')
 const mongoose = require('./db.ts');
 
 
-export function configureApp() {
-    const app = express();
+  const app = express();
 
-    app.use(cors())
-    app.use(eventContext());
+  app.use(cors())
+  // app.use(eventContext());
 
-    app.use(bodyParser.json({limit: '5mb', extended: true}))
-    app.use(bodyParser.urlencoded({limit: '5mb', extended: true}))
+  app.use(bodyParser.json({limit: '5mb', extended: true}))
+  app.use(bodyParser.urlencoded({limit: '5mb', extended: true}))
 
-    const apiRoutes = require('./routes/routes');
+  const apiRoutes = require('./routes/routes');
 
-    app.use('', apiRoutes);
+  app.use('', apiRoutes);
 
-    //Capture All 404 errors
-    app.use(function (req,res,next){
-      res.status(404).send('Error - Unable to find the requested resource!');
-    });
+  //Capture All 404 errors
+  app.use(function (req,res,next){
+    res.status(404).send('Error - Unable to find the requested resource!');
+  });
 
-    app.use((req, res, next) => {
-      req.socket.on('error', () => {});
-      next();
-    });
-    
-    return app;
-}
-
-//exports = app;
+  app.use((req, res, next) => {
+    req.socket.on('error', () => {});
+    next();
+  });
+  
+  export = app;
 // module.exports = app;
